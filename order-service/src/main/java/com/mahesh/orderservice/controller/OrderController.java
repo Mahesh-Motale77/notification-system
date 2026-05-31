3c792fe97a613346130138a20e76d5bc586e1718
Mahesh-Motale77 <motalemahesh77@gmail.com>
2026-05-31 14:40:02 +0530

    Added NotBlank validation for order Api

--- order-service/src/main/java/com/mahesh/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/mahesh/orderservice/controller/OrderController.java
@@ -5,11 +5,11 @@ import com.mahesh.orderservice.dto.response.OrderResponse;
 import com.mahesh.orderservice.service.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +30,7 @@ public class OrderController {
             description = "Creates a new order, saves to DB and publishes ORDER_CREATED event to Kafka"
     )
     @PostMapping(value = "/create")
-    public ResponseEntity<OrderResponse> createOrder(@RequestBody @Validated OrderRequest orderRequest){
+    public ResponseEntity<OrderResponse> createOrder(@RequestBody @Valid OrderRequest orderRequest){
         log.info("Inside OrderController --> createOrder() : orderRequest : {}", orderRequest);
         return ResponseEntity.ok(orderService.createOrder(orderRequest));
     }
--- order-service/src/main/java/com/mahesh/orderservice/dto/request/OrderRequest.java
+++ order-service/src/main/java/com/mahesh/orderservice/dto/request/OrderRequest.java
@@ -1,5 +1,6 @@
 package com.mahesh.orderservice.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
@@ -9,7 +10,13 @@ import org.apache.kafka.common.protocol.types.Field;
 @Builder
 @ToString
 public class OrderRequest {
+
+    @NotBlank(message = "UserId should not be blank")
     private String userId;
+
+    @NotBlank(message = "items should not be blank")
     private String items;
+
+    @NotBlank(message = "amount should not be blank")
     private Double amount;
 }
